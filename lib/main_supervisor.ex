29d33bb1f210b9cf38b4b9bce2a4c8ef682e4027
creestee <cristi.ccristi0@gmail.com>
2023-03-14 00:46:25 +0200

    add supervisor for reader and mediator actors

---
+++ lib/main_supervisor.ex
@@ -0,0 +1,29 @@
+defmodule TwitterSupervisor do
+  use Supervisor
+
+  def start_link() do
+    Supervisor.start_link(__MODULE__, name: __MODULE__)
+  end
+
+  def init(_init_arg) do
+    children = [
+      %{
+        id: :mediator,
+        start: {Mediator, :start_link, [[]]}
+      },
+      %{
+        id: :sse_client_1,
+        start: {SseClient, :start_link, [["localhost:4000/tweets/1", :mediator]]}
+      },
+      %{
+        id: :sse_client_2,
+        start: {SseClient, :start_link, [["localhost:4000/tweets/2", :mediator]]}
+      }
+      # {Mediator, []},
+      # {SseClient, ["localhost:4000/tweets/1", :mediator]}
+      # # {SseClient, ["localhost:4000/tweets/2", :mediator]}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
