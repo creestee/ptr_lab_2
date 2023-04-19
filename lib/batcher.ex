88c014c1f3ea1a9cd2a20ab5fc31021a4ecee5a2
rel <cristian.ionel@eu.rsystems.com>
2023-04-19 09:24:17 +0300

    remove print cast from batcher

--- lib/batcher.ex
+++ lib/batcher.ex
@@ -13,17 +13,12 @@ defmodule Batcher do
   def handle_cast({:collect, tweet}, {current_capacity, batch_size, batch}) do
     cond do
       current_capacity >= batch_size ->
-        GenServer.cast(self(), {:print, batch})
+        Logger.info "------------ BATCH PRINTING ------------"
+        IO.inspect Enum.with_index(batch, fn el, index -> "[#{index + 1}] - #{el}" end)
+        Logger.info "------------ BATCH FINISHED ------------"
         {:noreply, {0, batch_size, []}}
       true ->
         {:noreply, {current_capacity + 1, batch_size, [tweet | batch]}}
     end
-  end
-
-  def handle_cast({:print, batch}, state) do
-    Logger.info "------------ BATCH PRINTING ------------"
-    IO.inspect Enum.with_index(batch, fn el, index -> "[#{index + 1}] - #{el}" end)
-    Logger.info "------------ BATCH FINISHED ------------"
-    {:noreply, state}
   end
 end
